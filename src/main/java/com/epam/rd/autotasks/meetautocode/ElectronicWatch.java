f14901fc7ab7609fd06be5c804f373b97747fb43
Ashish7429 <ashish.11916604@lpu.in>
2023-01-18 23:43:21 +0530

    f

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,6 +6,24 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+        int s=seconds%60;
+        int h=seconds/60;
+        int m=h%60;
+        h=h/60;
+        if(h%24==0) {
+            h = 0;
+            System.out.println(h + "0:0" + m + ":0" + s);
+        }
+        else if(s<10 && m<10) {
+            System.out.println(h+":0"+m+":0"+s);
+        }else if(s<10 && m>10){
+            System.out.println(h+":"+m+":0"+s);
+        }else if(s>10 && m<10){
+            System.out.println(h+":0"+m+":"+s);
+        }
+        else{
+            System.out.println(h+":"+m+":"+s);
+        }
 
 
     }
